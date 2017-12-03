0ac67d8cd5e200470d504960535b4d79874e1775
Neelambari Verma <verma.ne@husky.neu.edu>
2017-12-03 12:21:45 -0500

    Changes in Pharma request Panel

--- DrugManagement/src/Business/DB4oUtil/DB4OUtil.java
+++ DrugManagement/src/Business/DB4oUtil/DB4OUtil.java
@@ -14,7 +14,7 @@ import com.db4o.ta.TransparentPersistenceSupport;
  */
 public class DB4OUtil {
 
-    private static final String FILENAME = "C:/DrugManagement.db4o"; // path to the data store
+    private static final String FILENAME = "src\\Resource\\DrugManagement.db4o"; // path to the data store
     private static DB4OUtil dB4OUtil;
     
     public synchronized static DB4OUtil getInstance(){
